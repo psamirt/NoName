282e2656e16201bca91c3ca88d958dc45515c769
psamirt <p_samir@hotmail.com>
2024-07-27 18:46:16 -0500

    primer push

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { createServer } from 'http';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import routes from './src/routes/index.js';
+import connection from './src/database/index.js';
+
+dotenv.config();
+
+const app = express();
+const server = createServer();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/', routes);
+
+app.get("/", (_req, res) => {
+    res.status(200).send("noname funcionanding")
+})
+
+connection()
+.then(()=> {
+    server.listen(/*puerto*/, () => {
+        console.log("Server running on port ...");
+    })
+})
+.catch((error)=> {
+    console.error("Error on connecting", error)
+})
---
+++ src/database/index.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+//falta definir la base de datos
+
+// const connection = async () => {
+//   try {
+//     await connect();
+//     console.log('Connected to ');
+//   } catch (error) {
+//     console.error('Error connecting to ', error);
+//   }
+// };
+
+export default connection;
